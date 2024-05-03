3a33d65668aab3f3efbaee22d9c05203a51f95de
kritikadubey16 <sonamdubey763@gmail.com>
2024-05-03 21:56:14 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ColorModeScript } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
+import { BrowserRouter  } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+  <App />
+</ChakraProvider>
+</BrowserRouter>
+);
+
+
