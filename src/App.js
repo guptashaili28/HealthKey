d3fa2f8e0482a6db242804f97229900b72123eab
kritikadubey16 <sonamdubey763@gmail.com>
2024-05-04 00:35:26 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { ChakraProvider, CSSReset, Box, extendTheme } from "@chakra-ui/react";
 import DashBoard from "./components/dashboard";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/header";
+import Hero from "./components/hero";
+import Footer from "./components/footer";
+
+import Register from "./components/register";
+
+import PublicDashboard from "./components/PublicDashboard";
 
 const theme = extendTheme(/* your theme configuration */);
 
@@ -13,7 +19,13 @@ const App = () => {
       <Header />
 
       <Routes>
-        <Route path="/" element={<DashBoard />} />
+        <Route path="/" element={<Hero />} />
+
+        <Route path="/dashboard" element={<DashBoard />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/publicdashboard" element={<PublicDashboard/>} />
       </Routes>
   
     </ChakraProvider>
